ff6dc0023b0c61b74c86ff172b7878cd0189d907
Maxim Kocheshkov <gror_godfroy@mail.ru>
2020-04-08 16:43:09 +0300

    Правки по доработке v.2

--- Logging_test.py
+++ Logging_test.py
@@ -4,27 +4,27 @@ import random
 from contextlib import contextmanager
 
 start_time = datetime.datetime.utcnow()
+
  
 @contextmanager
 def logger():
+  try:
     logger = logging.getLogger("password_generation")
     logger.setLevel(logging.INFO)
-
     fh = logging.StreamHandler()
- 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
-
     logger.addHandler(fh)
-    
     logger.info("Program started")
-    print('Пароль сгенерирован: ')
-    print(''.join([random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) for x in range(12)]))
-    logger.info("Done!")
     yield
     end_time = datetime.datetime.utcnow()
     print('Время работы: {}'.format(end_time - start_time))
+  finally:
+    logger.info("Done!")
+
  
 main = logger()
+
 with main:
-    pass
+    print('Пароль сгенерирован: ')
+    print(''.join([random.choice(list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')) for x in range(12)]))
